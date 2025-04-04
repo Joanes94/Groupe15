9a09767c119a964891c4e9f45bfd3e28b542e753
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-04-04 11:43:08 +0100

    Dossiers

--- frontend/src/composants/ButtonDossiers.jsx
+++ frontend/src/composants/ButtonDossiers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ButtonDossiers = ({ isActive, onClick }) => {
   return (
     <button 
-      className={`px-4 py-2 ${isActive ? 'bg-green-500' : 'bg-white'} border border-gray-300`} 
+      className={`px-4 lg:px-8 py-2 rounded-3xl font-bold text-lg ${isActive ? 'bg-[#269C26] text-white' : 'bg-white text-[#269C26]'} border-2 border-[#269C26]`} 
       onClick={onClick}
     >
       Dossiers
